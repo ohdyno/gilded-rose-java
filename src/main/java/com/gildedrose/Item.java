7d702c429ed5fa9309c9d87535483c8bb0ff9b67
xing <xing@integral.io>
2019-04-08 11:06:11 -0400

    Initial commit with ApprovalTests.

---
+++ src/main/java/com/gildedrose/Item.java
@@ -0,0 +1,16 @@
+package com.gildedrose;
+
+class Item {
+
+    final String name;
+
+    int sellIn;
+
+    int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+}
---
+++ src/test/java/come/gildedrose/GildedRoseTest.java
@@ -0,0 +1,9 @@
+package come.gildedrose;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+    @Test
+    public void scaffolding() {
+    }
+}
